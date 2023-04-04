d3672cd3df9f2d44bc0596409b3ddbd31164d734
martin0123478 <martin_agui97@outlook.es>
2023-04-03 21:25:23 -0600

    Mostrando resultados de busqueda

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState,useEffect,createContext } from "react";
 
 const NoticiasContext = createContext()
@@ -5,6 +6,20 @@ const NoticiasContext = createContext()
 const NoticiasProvider = ({children}) =>{
 
     const [categoria,setCategoria] =useState('general')
+
+    useEffect(()=>{
+        const consultarApi = async() =>{
+               try {
+                 const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+        console.log(url)
+        const {data} = await axios(url)
+        console.log(data)
+               } catch (error) {
+                console.log(error)
+               }
+        }
+        consultarApi()
+    },[categoria])
 
     const handleCategoria = e =>{
         setCategoria(e.target.value)
